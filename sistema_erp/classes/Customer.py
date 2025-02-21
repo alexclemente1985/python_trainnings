032702b32b2aaa223291de7844de9c505049e3cc
Alexandre Pinheiro <alexclemente1985@gmail.com>
2025-02-21 12:11:22 -0300

    Projeto Sistema ERP - conclusão database e testes insert e select all

--- sistema_erp/classes/Customer.py
+++ sistema_erp/classes/Customer.py
@@ -5,3 +5,4 @@ class Customer:
     name: str
     phone: str
     city: str
+    id_customer: int = None
--- sistema_erp/database.py
+++ sistema_erp/database.py
@@ -3,16 +3,17 @@ import sqlite3
 from typing import List
 from classes.Customer import Customer
 from classes.DBResults import DBResults
+import pandas as pd
 
 class Database_ERP:
     def __init__(self, name="erp.db") -> None:
         self.name = name
         self.database = self.create_db_path()
         self.create_table()
-    
+
     def connect(self):
         self.connection = sqlite3.connect(self.database)
-    
+
     def close_connection(self):
         try:
             self.connection.close()
@@ -42,7 +43,7 @@ class Database_ERP:
                                 CITY TEXT
                            )
                             """)
-            
+
         except Exception as e:
             print(e)
         finally:
@@ -65,7 +66,7 @@ class Database_ERP:
                     customer.city
                 )
             )
-        
+
         try:
             self.connect()
 
@@ -73,7 +74,7 @@ class Database_ERP:
             cursor.execute(f"""INSERT INTO Customers {table_fields} VALUES ({values_for_query})""", values)
 
             self.connection.commit()
-            
+
             result = DBResults(type="OK", msg="Cliente cadastrado com sucesso!")
             return result
 
@@ -83,7 +84,7 @@ class Database_ERP:
             self.connection.rollback()
 
             return result
-            
+
         except Exception as e:
             print(e)
 
@@ -102,21 +103,106 @@ class Database_ERP:
             cursor.execute("""SELECT * FROM Customers ORDER BY NAME""")
 
             customers_result = cursor.fetchall()
-            customers = List[Customer] = []
+            customers: List[Customer] = []
 
             for customer in customers_result:
                 c = Customer(
-                    name=customer[0],
-                    phone=customer[1],
-                    city=customer[2]
+                    id_customer=customer[0],
+                    name=customer[1],
+                    phone=customer[2],
+                    city=customer[3]
                 )
 
                 customers.append(c)
-            
+
             return customers
-        
+
         except Exception as e:
             print(e)
             return None
         finally:
             self.close_connection()
+
+    def delete_customer(self, id_customer):
+        try:
+            self.connect()
+            cursor = self.connection.cursor()
+            cursor.execute(f"""DELETE FROM Customers WHERE ID == '{id_customer}'""")
+            self.connection.commit()
+
+            return DBResults(type='OK', msg='Cliente removido com sucesso!')
+        except sqlite3.Error as e:
+            print(e)
+            result = DBResults(type="ERROR", msg=f"Falha na remoção dos dados: {e}.")
+            self.connection.rollback()
+            return result
+        except Exception as e:
+            print(e)
+            result = DBResults(type="ERROR", msg=f"Falha na remoção do registro do cliente id {id_customer}")
+            self.connection.rollback()
+            return result
+
+        finally:
+            self.close_connection()
+
+    def update_customer(self, customers: List[Customer]):
+        try:
+            self.connect()
+
+            cursor = self.connection.cursor()
+
+            for customer in customers:
+                cursor.execute(f"""
+                                UPDATE Customers
+
+                                SET
+                                NAME = '{customer.name}',
+                                PHONE = '{customer.phone}',
+                                CITY = '{customer.city}'
+
+                                WHERE ID_CUSTOMER = '{customer.id_customer}'
+                                """)
+
+                self.connection.commit()
+
+            result = DBResults(type='OK', msg="Cliente(s) atualizado(s) com sucesso!")
+            return result
+
+        except sqlite3.Error as e:
+            print(e)
+            result = DBResults(type="ERROR", msg=f"Erro na atualização dos dados: {e}")
+            self.connection.rollback()
+            return result
+        except Exception as e:
+            print(e)
+            result = DBResults(type="ERROR", msg=f"Falha no processo de atualização: {e}")
+            self.connection.rollback()
+            return result
+        finally:
+            self.connection.close()
+
+    def excel_report(self):
+        try:
+            self.connect()
+            customers = pd.read_sql_query("""SELECT * FROM Customers""", con = self.connection)
+            reports_path = Path.joinpath(Path(__file__).parent, "reports")
+
+            if not reports_path.exists():
+                Path.mkdir(reports_path)
+
+            xlsx_path = Path.joinpath(reports_path, "customers.xlsx")
+            customers.to_excel(xlsx_path, sheet_name="Clientes", index=False)
+
+            return DBResults(type="OK", msg="Relatório gerado com sucesso!")
+        except Exception as e:
+            print(e)
+            return DBResults(type="ERROR", msg="Erro na geração de relatório.")
+
+
+if __name__ == '__main__':
+   database = Database_ERP()
+   #customer: Customer = Customer(name="Jonas", phone="21 99226-6455", city="Paris")
+
+   #database.register_customer(customer=customer)
+
+   print(database.select_all_customers())
