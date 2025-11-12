7018213ab7a829106f0d6f22955f99d4a36af748
alexclemente1985 <alexclemente1985@gmail.com>
2025-11-12 09:03:50 -0300

    finalização módulo relatórios

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.registro_hora_extra.views import HoraExtraDelete, HoraExtraEdit, HoraExtraEditBase, HoraExtraList, HoraExtraNovo
+from apps.registro_hora_extra.views import ExportarCSV, ExportarExcel, HoraExtraDelete, HoraExtraEdit, HoraExtraEditBase, HoraExtraList, HoraExtraNovo
   
 
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path('editar-funcionario/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
     path('editar/<int:pk>/', HoraExtraEditBase.as_view(), name='update_hora_extra_base'),
     path('delete/<int:pk>/', HoraExtraDelete.as_view(), name='delete_hora_extra'),
-    
+    path('exportar-csv/', ExportarCSV.as_view(), name='exportar_csv'),
+    path('exportar-excel/', ExportarExcel.as_view(), name='exportar_excel'),
 ]
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,6 +1,10 @@
+import csv
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+import xlwt
 
 from apps.registro_hora_extra.forms import RegistroHoraExtraForm
 from apps.registro_hora_extra.models import RegistroHoraExtra
@@ -22,7 +26,7 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
 
-        return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+        return self.model.objects.filter(funcionario__empresa=empresa_logada)
     
 class HoraExtraEdit(UpdateView):
     model = RegistroHoraExtra
@@ -50,3 +54,68 @@ class HoraExtraEditBase(UpdateView):
 class HoraExtraDelete(DeleteView):
     model = RegistroHoraExtra
     success_url = reverse_lazy('list_hora_extra')
+
+
+class ExportarCSV(View):
+    model = RegistroHoraExtra
+    def get_success_url(self):
+        return reverse_lazy('list_hora_extra')
+    
+    def get(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="horas_extras.csv"'
+
+        writer = csv.writer(response)
+        
+        empresa_logada = self.request.user.funcionario.empresa
+        horas = self.model.objects.filter(funcionario__empresa=empresa_logada)
+
+        writer.writerow(['Motivo','Funcionário','Horas Restantes', 'Horas Retiradas'])
+        for hora in horas:
+            writer.writerow([hora.motivo, hora.funcionario.nome, hora.funcionario.total_horas_extra, hora.horas])
+        
+
+        return response
+
+class ExportarExcel(View):
+    model = RegistroHoraExtra
+
+    def get_success_url(self):
+        return reverse_lazy('list_hora_extra')
+    
+    def get(self, request):
+        response = HttpResponse(content_type='text/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="horas_extras.xls"'
+
+        writerB = xlwt.Workbook(encoding="utf-8")
+        writerS = writerB.add_sheet('Funcionários')
+
+        row_num = 0
+
+        font_style = xlwt.XFStyle()
+        font_style.font.bold = True
+
+        columns = ['Id','Motivo','Funcionário', 'Horas Extras']
+
+        for col_num in range(len(columns)):
+            writerS.write(row_num, col_num, columns[col_num], font_style)
+        
+        font_style = xlwt.XFStyle()
+
+        empresa_logada = self.request.user.funcionario.empresa
+        horas = self.model.objects.filter(funcionario__empresa=empresa_logada)
+
+        row_num = 1
+
+        for hora in horas:
+            writerS.write(row_num, 0, hora.id, font_style)
+            writerS.write(row_num, 1, hora.motivo, font_style)
+            writerS.write(row_num, 2, hora.funcionario.nome, font_style)
+            writerS.write(row_num, 3, hora.horas, font_style)
+
+            row_num += 1
+        
+        writerB.save(response)
+        
+
+        return response
